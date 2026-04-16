cf64f9709f7fe305254313b07ca0d9c062821730
MarianSimoiu <74770667+MarianSimoiu@users.noreply.github.com>
2026-04-16 15:15:20 +0300

    Update main.js

--- main.js
+++ main.js
@@ -1070,3 +1070,16 @@ const urlParams = new URLSearchParams(window.location.search);
 if (urlParams.get("admin") === "true") {
   authenticateAdmin();
 }
+(async () => {
+  const weekId = getCurrentWeekId();
+  onSnapshot(doc(db, "featuredAlbum", weekId), (docSnap) => {
+    if (docSnap.exists()) {
+      console.log("Featured album listener: album updated", docSnap.data());
+      updateFeaturedAlbum(docSnap.data());
+    } else {
+      console.log("Featured album listener: no featured album set for week", weekId);
+    }
+  }, (error) => {
+    console.error("Featured album listener error:", error);
+  });
+})();
